377a09162e973de5f60e8e4672261bb4fca68caf
Risheit <80658104+Risheit@users.noreply.github.com>
2022-07-18 00:35:33 -0500

    Add playRound function skeleton

--- rps.js
+++ rps.js
@@ -4,7 +4,25 @@ const PLAYABLES = {
     scissors: "SCISSORS"
 };
 
+function playRound(playerSelection, computerSelection) {
+    let result;
+    let context;
 
+    if (isTie(playerSelection, computerSelection)) {
+        result = "Tie";
+        context = "ties with"
+    }
+    else if (isLoss(playerSelection, computerSelection)) {
+        result = "Lose";
+        context = "loses to"
+    }
+    else {
+        result = "Won";
+        context = "beats"
+    }
+
+    return "You " + result + "! " + playerSelection + " " + context + " " + computerSelection;
+}
 
 function game() {
     const NUMBER_ROUNDS = 5;
