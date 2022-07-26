e621fbe442b8c3a1338c5afc09d7c9b8af9dc328
Risheit <80658104+Risheit@users.noreply.github.com>
2022-07-25 19:56:35 -0500

    Add RPS

--- rps.js
+++ rps.js
@@ -16,7 +16,7 @@ function isTie(playerSelection, computerSelection) {
 
 function isLoss(playerSelection, computerSelection) {
     return (playerSelection.toUpperCase() === PLAYABLES.rock &&
-            computerSelection.toUpperCase() === PLAYABLES.paper)
+        computerSelection.toUpperCase() === PLAYABLES.paper)
         || (playerSelection.toUpperCase() === PLAYABLES.paper &&
             computerSelection.toUpperCase() === PLAYABLES.scissors)
         || (playerSelection.toUpperCase() === PLAYABLES.scissors &&
@@ -57,7 +57,7 @@ function getPlayerChoice() {
     let choice = "";
     do {
         choice = prompt("What do you want to play? Rock/Paper/Scissors")
-                    .toUpperCase();
+            .toUpperCase();
     } while (!(Object.values(PLAYABLES).includes(choice)));
     return choice;
 }
@@ -74,5 +74,71 @@ function getPlayerChoice() {
 //     }
 // }
 
+function getPoints(status) {
+    return parseInt(status.slice(-1));
+}
 
-button = document.querySelector<button>(".start-game");
+function getUpdatedStatusString(status) {
+    let value;
+    value = getPoints(status);
+    value++;
+
+    return status.slice(0, status.length - 1) + value.toString();
+}
+
+function updateDisplay(outcome) {
+    const playerWins = document.querySelector("#player-wins");
+    const computerWins = document.querySelector("#computer-wins");
+
+    computerWins.textContent = computerWins.textContent.trimEnd();
+    playerWins.textContent = playerWins.textContent.trimEnd();
+
+    switch (outcome) {
+        case OUTCOMES.lose:
+            computerWins.textContent =
+                getUpdatedStatusString(computerWins.textContent);
+            break;
+        case OUTCOMES.win:
+            playerWins.textContent =
+                getUpdatedStatusString(playerWins.textContent);
+            break;
+    }
+}
+
+function displayOverlay(text) {
+    const overlay = document.querySelector("#overlay");
+    const winText = document.querySelector("#overlay > h1:first-of-type");
+
+    winText.textContent = text;
+    overlay.style.opacity = 1;
+    overlay.style.pointerEvents = "all";
+}
+
+function runRound(choice) {
+    updateDisplay(playRound(choice, getComputerChoice()));
+
+    if (getPoints(document.querySelector("#player-wins").textContent) >= 5)
+        displayOverlay("Player Wins!");
+    else if (getPoints(document.querySelector("#computer-wins").textContent)
+        >= 5)
+        displayOverlay("Bot Wins!");
+}
+
+function reset() {
+    document.querySelector("#player-wins").textContent = "Player: 0";
+    document.querySelector("#computer-wins").textContent = "Computer: 0";
+
+    const overlay = document.querySelector("#overlay");
+    overlay.style.opacity = 0;
+    overlay.style.pointerEvents = "none";
+}
+
+document.querySelector("#rock")
+    .addEventListener("click", () => runRound(PLAYABLES.rock));
+document.querySelector("#paper")
+    .addEventListener("click", () => runRound(PLAYABLES.paper));
+document.querySelector("#scissors")
+    .addEventListener("click", () => runRound(PLAYABLES.scissors));
+
+document.querySelector("#overlay > button:first-of-type")
+    .addEventListener("click", reset);
